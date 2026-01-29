4f2faa8cf1dd99506fb7bff7c8873009f09d9a58
Francis Guy-Stéphane NOGO <nogostephane@gmail.com>
2026-01-29 01:41:27 +0000

    Added stripe staging and payment fact table ;

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,18 +1,15 @@
-{{ config(materialized='table') }}
+with orders_enriched as (
 
-with customers as (
+    select
+        order_id,
+        customer_id,
+        order_date,
+        status
+    from {{ ref('stg_jaffle_shop__orders') }}
 
-    select * from {{ ref('stg_jaffle_shop__customers') }}
+    ),
 
-),
-
-orders as (
-
-     select * from {{ ref('stg_jaffle_shop__orders') }}
-
-),
-
-customer_orders as (
+    customer_orders as (
 
     select
         customer_id,
@@ -20,19 +17,27 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
 
-        count(order_id) as total_number_of_orders,
-
+        count(*) as total_number_of_orders,
         count_if(status = 'completed') as completed_orders,
         count_if(status != 'completed') as non_completed_orders
 
-    from orders
+    from orders_enriched
     group by customer_id
-
 ),
+
+customer_ltv as (
+
+    select
+        o.customer_id,
+        sum(case when p.status = 'success' then p.amount else 0 end) as lifetime_value
+    from {{ ref('stg_jaffle_shop__orders') }} o
+    left join {{ ref('stg_stripe__payments') }} p
+        on o.order_id = p.order_id
+    group by o.customer_id ),
 
 final as (
 
-    select 
+    select
         c.customer_id,
         c.first_name,
         c.last_name,
@@ -44,18 +49,20 @@ final as (
         coalesce(co.completed_orders, 0) as completed_orders,
         coalesce(co.non_completed_orders, 0) as non_completed_orders,
 
-        case 
+        coalesce(lv.lifetime_value, 0) as lifetime_value,
+
+        case
             when coalesce(co.total_number_of_orders, 0) = 0 then 'no_orders'
             when coalesce(co.total_number_of_orders, 0) = 1 then 'one_time_customer'
             when coalesce(co.total_number_of_orders, 0) between 2 and 4 then 'repeat_customer'
             else 'loyal_customer'
         end as customer_segment
 
-    from customers c
+    from {{ ref('stg_jaffle_shop__customers') }} c
     left join customer_orders co
         on c.customer_id = co.customer_id
-
+    left join customer_ltv lv
+        on c.customer_id = lv.customer_id
 )
 
-select *
-from final
+select * from final
